2df9ba67cc070ef0501646682e4371db1c6045ed
Aine Evans <evansaine1@gmail.com>
2025-10-12 17:22:29 +0300

    Update .gitignore to include package-lock.json and remove package-lock.json files from client, backend, and root directories

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import SignUp from './components/SignUp';
-import StudentPayment from './components/StudentPayment';
+import StudentPayment from './views/components/StudentPayment';
 
 function App() {
-  const [showSignUp, setShowSignUp] = useState(false);
   const [showPayment, setShowPayment] = useState(false);
-  const [showLogin, setShowLogin] = useState(false);
-
-  if (showSignUp) {
-    return (
-      <div className="App">
-        <SignUp />
-      </div>
-    );
-  }
 
   if (showPayment) {
     return (
@@ -35,9 +24,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Worldz</h1>
-      <button onClick={() => setShowSignUp(true)}>Sign Up</button>
-      <button onClick={() => setShowPayment(true)} style={{ marginLeft: '10px' }}>Student Payment</button>
-      <button onClick={() => setShowLogin(true)} style={{ marginLeft: '10px' }}></button>
+      <button onClick={() => setShowPayment(true)}>Student Payment</button>
     </div>
   );
 }
