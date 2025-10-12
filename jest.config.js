467a7feef9c328a3a7876820c4b1b487dc4c05ee
Musiimemarthatrisha <90515537+Musiimemarthatrisha@users.noreply.github.com>
2025-10-12 13:13:50 +0300

    added app.js file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  testMatch: ['**/tests/**/*.test.js'],
+  modulePathIgnorePatterns: ['<rootDir>/backend/node_modules', '<rootDir>/client']
+};
+
