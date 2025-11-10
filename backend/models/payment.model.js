5466e5b4d3aad5d50cbcc2c6b504ddc498847299
Musiimemarthatrisha <90515537+Musiimemarthatrisha@users.noreply.github.com>
2025-11-10 06:46:09 +0300

    I have modified the payments model

---
+++ backend/models/payment.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const paymentSchema = new mongoose.Schema(
+  {
+    booking: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Booking',
+      required: true,
+    },
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['Mobile Money', 'Credit Card', 'Bank Transfer'],
+      required: true,
+    },
+    transactionId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    status: {
+      type: String,
+      enum: ['Pending', 'Completed', 'Failed'],
+      default: 'Pending',
+    },
+  },
+  { timestamps: true }
+);
+
+const Payment = mongoose.model('Payment', paymentSchema);
+export default Payment;
